6a1fc576d00b392664ab574436bd905d5e1e0ffa
alumno <alumno@A302PC35>
2023-10-06 09:04:45 +0200

    Cudros de Texto Empezando

--- Tema2-CuadrosTexto/MainWindow.xaml.cs
+++ Tema2-CuadrosTexto/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,59 @@ namespace Tema2_CuadrosTexto
         public MainWindow()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void AyudaNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if(e.Key == Key.F1) {
+                if(ayudaNombre.Text == "") {
+                    ayudaNombre.Text = "Nombre del cliente";
+                    
+                }
+                 else if(ayudaNombre.Text == "Nombre del cliente") {
+                    ayudaNombre.Text = "";
+                }       
+       
+            }
+            
+                
+        }
+
+        private void AyudaApellido_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                if (ayudaApellido.Text == "")
+                {
+                    ayudaApellido.Text = "Apellido del cliente";
+
+                }
+                else if (ayudaApellido.Text == "Apellido del cliente")
+                {
+                    ayudaApellido.Text = "";
+                }
+
+            }
+        }
+
+        private void validarEdad_KeyDown(object sender, KeyEventArgs e)
+        {
+            Regex regex = new Regex("^[0-9]+?");
+
+            if(e.Key == Key.F2)
+            {
+                if (!regex.IsMatch(recuadroEscrituraEdad.Text)) {
+                    validarEdad.Text = "Edad Incorrecta";
+                    validarEdad.Foreground = Brushes.Red;
+                }
+                else
+                {
+                    validarEdad.Text = "";
+                }
+            }
         }
     }
 }
